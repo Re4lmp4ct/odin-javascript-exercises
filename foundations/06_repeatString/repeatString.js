db54b4fc747bf4bc76b0b922bad99ca525c09b1a
Re4lmp4ct <haziqhamsyari@gmail.com>
2026-01-07 14:03:09 +0800

    - Completed Loops and Arrays

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(message, repetitions) {
+    if (repetitions < 0)
+    {
+        return 'ERROR';
+    }
+    else
+    {
+        let repeatedMessage = '';
+        for (let i = 0; i < repetitions; ++i)
+        {
+            repeatedMessage += message;
+        }
+        return repeatedMessage;
+    }
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const CELSIUS_TO_FAHRENHEIT_MULTIPLIER = 9.0 / 5.0;
+const FAHRENHEIT_TO_CELSIUS_MULTIPLIER = 5.0 / 9.0;
+const CONVERSION_CONSTANT = 32.0;
+
+const roundToOneDecimalPlace = function(value) {
+  return Math.round(value * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(fahrenheit) {
+  return roundToOneDecimalPlace((fahrenheit - CONVERSION_CONSTANT) * FAHRENHEIT_TO_CELSIUS_MULTIPLIER);
+};
+
+const convertToFahrenheit = function(celsius) {
+  return roundToOneDecimalPlace(celsius * CELSIUS_TO_FAHRENHEIT_MULTIPLIER + CONVERSION_CONSTANT);
 };
 
 // Do not edit below this line
